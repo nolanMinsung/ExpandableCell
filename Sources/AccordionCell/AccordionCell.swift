2d30eac446b0c5ac4ed7dac16e61f78a79d6890c
Minsung Kim <mskim4048@naver.com>
2025-03-22 16:54:42 +0900

    ExpandableCell 수정

--- Sources/AccordionCell/AccordionCell.swift
+++ Sources/AccordionCell/AccordionCell.swift
@@ -5,39 +5,43 @@ import UIKit
 
 open class ExpandableCell: UICollectionViewCell {
     
-    private var isExpanded = false
-    private let expandableView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .lightGray
-        view.isHidden = true
-        return view
-    }()
+    private(set) var originalView = UIView()
+    private(set) var additionalView = UIView()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(expandableView)
-        // 추가 콘텐츠 레이아웃 설정
-        expandableView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            expandableView.topAnchor.constraint(equalTo: contentView.bottomAnchor),
-            expandableView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
-            expandableView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
-            expandableView.heightAnchor.constraint(equalToConstant: 0) // 초기 높이 0
-        ])
+    private lazy var shrinkedBottomConstraint = originalView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    private lazy var expandedBottomConstraint = additionalView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    
+    override open var isSelected: Bool {
+        didSet { setAppearance() }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setViewHierarchy()
+        setupLayoutConstraints()
     }
     
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func toggleAccordion() {
-        isExpanded.toggle()
-        
-        // 애니메이션을 통해 펼쳐지거나 접히는 효과 적용
-        UIView.animate(withDuration: 0.3) {
-            self.expandableView.isHidden = !self.isExpanded
-            self.expandableView.frame.size.height = self.isExpanded ? 100 : 0 // 높이 조절
-            self.layoutIfNeeded() // 레이아웃 업데이트
-        }
+    private func setViewHierarchy() {
+        contentView.addSubview(originalView)
+        contentView.addSubview(additionalView)
     }
+    
+    private func setupLayoutConstraints() {
+        originalView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        originalView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
+        originalView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0).isActive = true
+//        originalView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    private func setAppearance() {
+        expandedBottomConstraint.isActive = isSelected
+        shrinkedBottomConstraint.isActive = !isSelected
+        contentView.layoutIfNeeded()
+    }
+    
 }
