e2cd60824eba8c7deab70105229e214aa9b88f7f
Minsung Kim <mskim4048@naver.com>
2025-03-23 15:38:27 +0900

    AccCell, AccCellCollectionView 타입 다시 정의

---
+++ Sources/AccordionCell/AccCell.swift
@@ -0,0 +1,115 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import UIKit
+
+protocol WidthAdoptable {
+    func adoptWidth(_ width: CGFloat)
+}
+
+open class AccCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    private let collectionViewHorizontalSectionInset: CGFloat = 24
+    private lazy var widthConstraint = contentView.widthAnchor.constraint(
+        equalToConstant: (UIScreen.main.bounds.width - collectionViewHorizontalSectionInset * 3)
+    )
+    
+    private lazy var expandedBottomConstraint = lowerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    private lazy var shrinkedBottomConstraint = upperView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    
+    override public var isSelected: Bool {
+        didSet { setAppearance() }
+    }
+    
+    //MARK: - UI Properties
+    
+    let upperView = UIView()
+    let lowerView = UIView()
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViewHierarchy()
+        setupStyle()
+        setupLayout()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension AccCell {
+    
+    //MARK: - Private Func
+    
+    private func setupViewHierarchy() {
+        contentView.backgroundColor = .lightGray
+        contentView.addSubview(upperView)
+        contentView.addSubview(lowerView)
+    }
+    
+    private func setupStyle() {
+        
+        upperView.clipsToBounds = true
+        upperView.backgroundColor = .orange
+        lowerView.clipsToBounds = true
+        lowerView.backgroundColor = .lightGray
+    }
+    
+    private func setupLayout() {
+        widthConstraint.priority = .defaultHigh
+        widthConstraint.isActive = true
+        
+        NSLayoutConstraint.activate([
+            upperView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            upperView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            upperView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+        
+//        upperView.snp.makeConstraints { make in
+//            make.top.horizontalEdges.equalToSuperview()
+//        }
+        
+        NSLayoutConstraint.activate([
+            lowerView.topAnchor.constraint(equalTo: contentView.bottomAnchor),
+            lowerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            lowerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+        
+//        lowerView.snp.makeConstraints { make in
+//            make.top.equalTo(upperView.snp.bottom)
+//            make.horizontalEdges.equalToSuperview()
+//        }
+        
+        expandedBottomConstraint.priority = .defaultLow
+        shrinkedBottomConstraint.priority = .defaultLow
+        
+        expandedBottomConstraint.isActive = isSelected
+        shrinkedBottomConstraint.isActive = !isSelected
+        lowerView.isHidden = !isSelected
+    }
+    
+    private func setAppearance() {
+        lowerView.isHidden = !isSelected
+        expandedBottomConstraint.isActive = isSelected
+        shrinkedBottomConstraint.isActive = !isSelected
+        contentView.updateConstraints()
+        contentView.layoutIfNeeded()
+    }
+    
+}
+
+extension AccCell: WidthAdoptable {
+    
+    func adoptWidth(_ width: CGFloat) {
+        self.widthConstraint.constant = width
+        setAppearance()
+    }
+    
+}
---
+++ Sources/AccordionCell/AccCellCollectionView.swift
@@ -0,0 +1,99 @@
+//
+//  AccordionCollectionView.swift
+//  AccordionCell
+//
+//  Created by 김민성 on 3/23/25.
+//
+
+import UIKit
+
+//import Then
+
+open class AccCellCollectionView: UICollectionView {
+    
+    private let cellSelectionOperationQueue = OperationQueue()
+    
+    init() {
+//        let collectionViewHorizontalInset: CGFloat = 24
+        let collectionViewHorizontalInset: CGFloat = 0
+        let collectionViewVerticalInset: CGFloat = 20
+        let itemWidth = floor(UIScreen.main.bounds.width - collectionViewHorizontalInset * 2)
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = .init(
+            top: collectionViewVerticalInset,
+            left: collectionViewHorizontalInset,
+            bottom: 40,
+            right: collectionViewHorizontalInset
+        )
+        flowLayout.minimumLineSpacing = 16
+        flowLayout.minimumInteritemSpacing = 100
+        //            flowLayout.estimatedItemSize = CGSize(width: itemWidth, height: 125)
+        flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        
+        super.init(frame: .zero, collectionViewLayout: flowLayout)
+        
+        setupStyle()
+        setupDelegates()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupStyle() {
+        backgroundColor = .darkGray
+        indicatorStyle = .black
+        contentInsetAdjustmentBehavior = .never
+        scrollIndicatorInsets = .zero
+        allowsMultipleSelection = true
+    }
+    
+    func setupDelegates() {
+        self.delegate = self
+    }
+    
+}
+
+extension CollectionView: UICollectionViewDelegate {
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        
+        guard collectionView.cellForItem(at: indexPath) is AccCell else { return true }
+        let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1)
+        animator.addAnimations {
+            if collectionView.indexPathsForSelectedItems?.contains(indexPath) ?? false {
+                collectionView.deselectItem(at: indexPath, animated: false)
+            } else {
+                collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+            }
+            collectionView.performBatchUpdates(nil)
+        }
+        
+        let animationOperation = BlockOperation {
+            DispatchQueue.main.async { animator.startAnimation() }
+        }
+        
+        cellSelectionOperationQueue.addOperation(animationOperation)
+        return false
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+        // deselect는 allowsMultipleSelection = true 인 경우에만 해당
+        guard collectionView.allowsMultipleSelection else { return false }
+        let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1)
+        animator.addAnimations {
+            collectionView.deselectItem(at: indexPath, animated: false)
+            collectionView.performBatchUpdates(nil)
+        }
+        
+        let animationOperation = BlockOperation {
+            DispatchQueue.main.async { animator.startAnimation() }
+        }
+        
+        cellSelectionOperationQueue.addOperation(animationOperation)
+        return false
+    }
+    
+}
